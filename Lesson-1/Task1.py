33ca0d7b4add628dd425a8a82b2a5fcf183e301f
svetaosmonova <svetaosmonova.83@gmail.com>
2025-02-18 20:49:19 +0300

    Добавлен скрипт на добавление имени

--- Lesson-1/Task1.py
+++ Lesson-1/Task1.py
@@ -1,25 +1,7 @@
-my_heigh = 160
-print(my_height)
+
 
 
 my_name = "Sveta"
 my_name = "Sveta Osmonova"
 print(my_name)
 
-pet_name = input("Киреше")
-print("Киреше - " + pet_name)
-
-def print_python():
-    print_python("«Учу Python!»")
-
-print_python()
-
-def print_letter(let):
-    print(let, end='')
-
-print_letter('S')
-print_letter('k')
-print_letter('y')
-print_letter('P')
-print_letter('r')
-print_letter('o')
