d07f7c1540c7d5824401defff9a65fc3eab66e68
svetaosmonova <svetaosmonova.83@gmail.com>
2025-02-18 20:34:20 +0300

    Добавлен скрипт на добавление возраста

--- Lesson-1/Task2.py
+++ Lesson-1/Task2.py
@@ -1,10 +1,3 @@
-print ('Привет мир! Сегодня лучший день')
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+my_age = 40
+my_age = my_age + 3
+print(my_age)
