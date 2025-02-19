7b96f8ef84a2f0d5a5da5c03fe3ccc28f89bf447
svetaosmonova <svetaosmonova.83@gmail.com>
2025-02-19 18:15:09 +0300

    Добавлен скрипт Имя

--- Lesson-1/Task3.py
+++ Lesson-1/Task3.py
@@ -1,6 +1,6 @@
 # Получение пользовательского ввода
-first_name = input("Света: ")
-last_name = input("Осмонова: ")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 
 # Вывод на экран
 print("Вас зовут: " + last_name + " " + first_name)
